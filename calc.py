6031dabfb5f341fe8c4099b90cae1ac99fe3c1a2
Hubbard <rocktut@icloud.com>
2025-10-21 14:25:27 -0600

    add testing

--- calc.py
+++ calc.py
@@ -1,18 +1,18 @@
 import math
 print ("welcome to the mehthematical calc (short for calculator)")
 def addition(x, y):
-    return x + y
+    return round(x + y,10)
 def subtraction(x, y):
-    return x - y
+    return round(x - y,10)
 def multiplication(x, y):
-    return x * y
+    return round(x * y,10)
 def division(x, y):
     if y == 0:
         return False
     else:
-        return x / y
+        return round(x / y,10)
 def exponent(x, y):
-    return pow(x, y)
+    return round(pow(x, y),10)
 def real_number_calculator(num):
     try:
         super_num = float(num)
@@ -21,57 +21,58 @@ def real_number_calculator(num):
         return False
     else:
         return True
-super_working = False
-while True == True:
+if __name__ == "__main__":
+    super_working = False
     while True == True:
-        print ("what operation do you want, please pick a easy one")
-        the_ops_tryna_swiss_cheese_me = input(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Exponent \n ")
-        if the_ops_tryna_swiss_cheese_me == "1" or the_ops_tryna_swiss_cheese_me == "2" or the_ops_tryna_swiss_cheese_me == "3" or the_ops_tryna_swiss_cheese_me == "4" or the_ops_tryna_swiss_cheese_me == "5":
-            break
-        else:
-            print ("how hard could it be to type a number")
-    while True == True:
-        x = input ("what should the first number be? \n ")
-        super_working = real_number_calculator(x)
-        if super_working == True:
-            x = float(x)
-            break
-        else:
-            print ("make my job easy, give me a real number")    
-    while True == True:
-        y = input ("i guess i need a second number too \n ")
-        super_working = real_number_calculator(y)
-        if super_working == True:
-            y = float(y)
-            break
-        else:
-            print ("make my job easy, give me a real number")
-    if the_ops_tryna_swiss_cheese_me == "1":
-        result = addition(x, y)
-        print (f"your number is {result}")
-    elif the_ops_tryna_swiss_cheese_me == "2":
-        result = subtraction(x, y)
-        print (f"your number is {result}")
-    elif the_ops_tryna_swiss_cheese_me == "3":
-        result = multiplication(x, y)
-        print (f"your number is {result}")
-    elif the_ops_tryna_swiss_cheese_me == "4":
-        result = division(x, y)
-        if result == False:
-            print ("you cant divide by 0, you really cant do anything right")
-        else:
+        while True == True:
+            print ("what operation do you want, please pick a easy one")
+            the_ops_tryna_swiss_cheese_me = input(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Exponent \n ")
+            if the_ops_tryna_swiss_cheese_me == "1" or the_ops_tryna_swiss_cheese_me == "2" or the_ops_tryna_swiss_cheese_me == "3" or the_ops_tryna_swiss_cheese_me == "4" or the_ops_tryna_swiss_cheese_me == "5":
+                break
+            else:
+                print ("how hard could it be to type a number")
+        while True == True:
+            x = input ("what should the first number be? \n ")
+            super_working = real_number_calculator(x)
+            if super_working == True:
+                x = float(x)
+                break
+            else:
+                print ("make my job easy, give me a real number")    
+        while True == True:
+            y = input ("i guess i need a second number too \n ")
+            super_working = real_number_calculator(y)
+            if super_working == True:
+                y = float(y)
+                break
+            else:
+                print ("make my job easy, give me a real number")
+        if the_ops_tryna_swiss_cheese_me == "1":
+            result = addition(x, y)
             print (f"your number is {result}")
-    elif the_ops_tryna_swiss_cheese_me == "5":
-        result = exponent(x, y)
-        print (f"your number is {result}")
-    print ("please say you dont want to do more math")
-    while True == True:
-        again = input (" 1. Do another calculation \n 2. Exit \n ")
-        if again == "2":
-            print ("screw off then")
-            exit()
-        elif again == "1":
-            break
-        else:
-            print ("i'm just gonna pretend you said 2")
-            exit()
+        elif the_ops_tryna_swiss_cheese_me == "2":
+            result = subtraction(x, y)
+            print (f"your number is {result}")
+        elif the_ops_tryna_swiss_cheese_me == "3":
+            result = multiplication(x, y)
+            print (f"your number is {result}")
+        elif the_ops_tryna_swiss_cheese_me == "4":
+            result = division(x, y)
+            if result == False:
+                print ("you cant divide by 0, you really cant do anything right")
+            else:
+                print (f"your number is {result}")
+        elif the_ops_tryna_swiss_cheese_me == "5":
+            result = exponent(x, y)
+            print (f"your number is {result}")
+        print ("please say you dont want to do more math")
+        while True == True:
+            again = input (" 1. Do another calculation \n 2. Exit \n ")
+            if again == "2":
+                print ("screw off then")
+                exit()
+            elif again == "1":
+                break
+            else:
+                print ("i'm just gonna pretend you said 2")
+                exit()
